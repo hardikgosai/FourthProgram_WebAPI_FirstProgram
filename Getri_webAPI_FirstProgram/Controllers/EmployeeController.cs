09224472efe760a30ffcbbd643ca283fb21c6f12
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-13 21:56:58 +0530

    Add project files.

---
+++ Getri_webAPI_FirstProgram/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Getri_webAPI_FirstProgram.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult FetchEmployee()
+        {
+            return Ok("This is first API call.");
+        }
+    }
+}
